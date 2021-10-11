9f3bbac97a4306818cd91cc0ac3a8afe33f3f7cf
Fisher121 <58854288+Fisher121@users.noreply.github.com>
2021-10-11 21:37:08 +0300

    Add files via upload

--- P3/NET2P1/NET2P1/Program.cs
+++ P3/NET2P1/NET2P1/Program.cs
@@ -22,8 +22,21 @@ namespace NET2P1
             /*            Football football = new Football();
                         var listGoalFor = football.getGoalForList(Parser, matrix);
                         var listGoalAgainst = football.getGoalAgainstList(Parser, matrix);
-                        Console.WriteLine(matrix.stringValues[Parser.getMinimumDifference(listGoalFor, listGoalAgainst)]);*/
+                        Console.WriteLine("Echipa care are golaverajul cel mai mic este: " + matrix.stringValues[Parser.getMinimumDifference(listGoalFor, listGoalAgainst)]);*/
 
         }
     }
 }
+/*
+ *  Intrebarea 1:
+ *  Aspectele de design luate initial ne-au ajutat deoarece am avut un model general dupa care sa proiectam programul final
+ *  
+ *  Intrebarea 2:
+ *  Da, deoarece am inceput tranzitia spre un program care poate citi fisiere asemanatoare inca din partea a 2 a
+ *  
+ *  Intrebarea 3:
+ *  Factorizarea programului in asa fel incat sa avem cat mai putin cod este, in general, un lucru bun atat din punct de vedere al eficientei cat si al
+ *  lizibilitatii codului. Insa, uneori acest lucru poate complica intelegerea codului de catre alti programatori. In cazul nostru, consideram ca am reusit
+ *  sa creem un cod lizibil. Programul ar trebui sa functioneze si pentru fisiere diferite, dar care au aceeasi structura de baza, cu conditia de a creea
+ *  un model nou.
+ */
