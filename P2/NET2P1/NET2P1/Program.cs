3c0893d0ee65fa6d08926cfc2d5104b1fd10eb00
Fisher121 <58854288+Fisher121@users.noreply.github.com>
2021-10-11 13:58:34 +0300

    Add files via upload

---
+++ P2/NET2P1/NET2P1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+
+namespace NET2P1
+{                                                                           
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            parser Parser = new parser("football.dat");
+            double[,] matrix = Parser.parse();
+            for (int z = 0; z < Parser.getRowCount(); z++)
+            {
+                for (int k = 0; k < Parser.getColumnCount(); k++)
+                {
+                    Console.Write(matrix[z, k]);
+                    Console.Write(',');
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
